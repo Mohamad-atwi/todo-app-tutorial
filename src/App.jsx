ab25d8990a4f45873e11cb210908cb4183a20945
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-06-03 00:57:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TodoList from './components/TodoList'
+import "./index.css"
+
+function App() {
+  return (
+    <div className='bg-gray-800 h-screen overflow-scroll'>
+     <TodoList />
+    </div>
+  )
+}
+
+export default App
