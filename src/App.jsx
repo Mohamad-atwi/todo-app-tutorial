db2546e05b4adea8518e6417fe23cfa761c05de3
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-06-06 15:52:31 +0200

    fix styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import "./index.css"
 
 function App() {
   return (
-    <div className='bg-gray-800 h-screen overflow-scroll'>
+    <div className='bg-gray-800 h-screen'>
      <TodoList />
+     <a href='https://pobierak.jeja.pl/images/d/e/c/650668_monitoring-w-turcji.jpg' target='_blank' rel='noreferrer' className='px-2 py-1 bg-gray-800 text-gray-800 rounded-md absolute right-4 bottom-4 hover:bg-red-500'>🤍</a>
     </div>
   )
 }
